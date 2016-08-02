c73b28ae027367a6da7fc816f0fc920f70e06b10
jreynolds4 <jon.rey22@gmail.com>
2016-08-02 16:16:33 -0400

    Final release

---
+++ root/index.php
@@ -0,0 +1,125 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="/photos/slizzatrism.ico">
+
+    <title>$lizzatri$m</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <!-- link href="/css/temp.css" rel="stylesheet" -->
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="//assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="/js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <link rel="stylesheet" href="/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/css/font-awesome.css">
+
+    <script src="https://use.fontawesome.com/b026176100.js"></script>
+
+    <link href="/css/slizzatrism.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <div class="masthead container centered ">
+      <div class="inner">
+        <h3 class="masthead-brand">
+          <a href="http://www.instagram.com/rasnebyu" target="_blank"><span><i class="fa fa-instagram"></i></span></a>
+          <a href="http://www.soundcloud.com/rasnebyu" target="_blank"><span><i class="fa fa-soundcloud"></i></span></a>
+          <a href="https://www.youtube.com/user/LexThunderTV" target="_blank"><span><i class="fa fa-youtube-play"></i></span></a>
+          <a href="http://www.twitter.com/rasnebyu" target="_blank"><span><i class="fa fa-twitter"></i></span></a>
+          <a href="http://www.facebook.com/rasnebyu" target="_blank"><span><i class="fa fa-facebook"></i></span></a>
+        </h3>
+
+        <nav>
+          <ul class="nav masthead-nav">
+            <li class="active"><a href="./">Home</a></li>
+            <li><a href="./media">Media</a></li>
+            <li><a href="./roster">Roster</a></li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+
+    <div class="container">
+      <!-- Modal -->
+      <div class="modal fade" id="contactForm" role="dialog">
+        <div class="modal-dialog">
+
+          <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal">&times;</button>
+              <h4 class="modal-title">Contact Form</h4>
+            </div>
+            <div class="modal-body">
+              <form action="" method="post" enctype="text/plain">
+                <div class="modal-label">Name:<br></div>
+                <input type="text" name="name"><br>
+                <div class="modal-label">E-mail:<br></div>
+                <input type="text" name="email"><br>
+                <div class="modal-label">Message:<br></div>
+                <textarea name="message" rows="3" cols="25"></textarea><br><br>
+                <input class="modal-label" type="submit" value="Send">
+                <input class="modal-label" type="reset" value="Reset">
+              </form>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+    </div>
+
+    <div class="container main-content centered" role="main">
+
+      <div class="home-heading">SLIZZATRISM</div>
+      <p class="lead">
+        The Official Website of The Washington Slizzards
+      </p
+
+      <div class="centered">
+        <div class="col-sm-3"></div>
+        <div class="col-sm-6">
+          <iframe width="100%" height="250" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/276495528&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
+        </div>
+        <div class="col-sm-3"></div>
+      </div>
+
+      <footer class="centered"> Webmaster: <a href="MAILTO:jon.rey22@gmail.com">Dr. Jrey</a> </footer>
+
+
+    </div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
---
+++ root/roster.php
@@ -0,0 +1,246 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="/photos/slizzatrism.ico">
+
+    <title>$lizzatri$m</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <!-- link href="/css/temp.css" rel="stylesheet" -->
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="//assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="/js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <link rel="stylesheet" href="/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/css/font-awesome.css">
+
+    <script src="https://use.fontawesome.com/b026176100.js"></script>
+
+    <link href="/css/slizzatrism.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <!-- Header -->
+
+    <div class="masthead container centered ">
+      <div class="inner">
+        <h3 class="masthead-brand">
+          <a href="http://www.instagram.com/rasnebyu" target="_blank"><span><i class="fa fa-instagram"></i></span></a>
+          <a href="http://www.soundcloud.com/rasnebyu" target="_blank"><span><i class="fa fa-soundcloud"></i></span></a>
+          <a href="https://www.youtube.com/user/LexThunderTV" target="_blank"><span><i class="fa fa-youtube-play"></i></span></a>
+          <a href="http://www.twitter.com/rasnebyu" target="_blank"><span><i class="fa fa-twitter"></i></span></a>
+          <a href="http://www.facebook.com/rasnebyu" target="_blank"><span><i class="fa fa-facebook"></i></span></a>
+        </h3>
+
+        <nav>
+          <ul class="nav masthead-nav">
+            <li><a href="./">Home</a></li>
+            <li><a href="./media">Media</a></li>
+            <li class="active"><a href="./roster">Roster</a></li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+
+    <div class="container">
+      <!-- Modal -->
+      <div class="modal fade" id="contactForm" role="dialog">
+        <div class="modal-dialog">
+
+          <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal">&times;</button>
+              <h4 class="modal-title">Contact Form</h4>
+            </div>
+            <div class="modal-body">
+              <form action="" method="post" enctype="text/plain">
+                <div class="modal-label">Name:<br></div>
+                <input type="text" name="name"><br>
+                <div class="modal-label">E-mail:<br></div>
+                <input type="text" name="email"><br>
+                <div class="modal-label">Message:<br></div>
+                <textarea name="message" rows="3" cols="25"></textarea><br><br>
+                <input class="modal-label" type="submit" value="Send">
+                <input class="modal-label" type="reset" value="Reset">
+              </form>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+    </div>
+
+    <!-- Content -->
+
+    <div class="container main-content centered" role="main">
+
+      <div class="site-heading"><a href="./">SLIZZATRISM</a></div>
+
+      <div class="slizz-description">
+        Washington Slizzards are a collective of creative young adults based in
+        Washington DC. Co-founded by Adon of 3rd Wrld Design and Ras Nebyu in 2011.
+        The Slizzards have slowly become one of DC’s most intriguing brands with
+        exceptional merchandise and stand out music led by The Arckitech (producer/engineer),
+        DJ K-Meta (disc jockey/A<span class="ampersand">& </span>R) and Ras Nebyu
+        (creative director/recording artist).
+      </div>
+
+      <!-- Roster -->
+
+      <div class="row media-elements">
+        <div class="col-sm-3 media-padding">
+          <div class="roster-img"><img src="./photos/neb.png" height="171" width="171" /></div>
+          <div>RAS NEBYU</div>
+          <div>Recording Artist, co-founder</div>
+          <div>
+            <a href="https://instagram.com/rasnebyu"><span><i class="fa fa-instagram"></i></span></a>
+            <a href="https://twitter.com/rasnebyu"><span><i class="fa fa-twitter"></i></span></a>
+            <a href="https://soundcloud.com/rasnebyu"><span><i class="fa fa-soundcloud"></i></span></a>
+          </div>
+        </div>
+        <div class="col-sm-3 media-padding">
+          <div class="roster-img"><img src="./photos/adonai-pic.jpg" width="171" height="171" /></div>
+          <div>ADONAY G (of 3rd Wrld)</div>
+          <div>Visual Artist, co-founder</div>
+          <div>
+            <a href="https://instagram.com/AdonayGhirmay_"><span><i class="fa fa-instagram"></i></span></a>
+            <a href="https://twitter.com/3rdwrlddesign"><span><i class="fa fa-twitter"></i></span></a>
+          </div>
+        </div>
+        <div class="col-sm-3 media-padding">
+          <div class="roster-img"><img src="./photos/arkitech-pic.jpg" width="173" height="171" /></div>
+          <div>THE ARCKITECH</div>
+          <div>Producer, Engineer, Recording Artist</div>
+          <div>
+            <a href="https://instagram.com/thearckitech"><span><i class="fa fa-instagram"></i></span></a>
+            <a href="https://twitter.com/thearckitech"><span><i class="fa fa-twitter"></i></span></a>
+            <a href="https://soundcloud.com/thearckitech"><span><i class="fa fa-soundcloud"></i></span></a>
+          </div>
+        </div>
+        <div class="col-sm-3 media-padding">
+          <div class="roster-img"><img src="./photos/kmeta-pic.jpg" height="171" width="193" /></div>
+          <div>DJ K-META</div>
+          <div>Disc Jockey, A<span class="ampersand">& </span>R</div>
+          <div>
+            <a href="https://instagram.com/djkmeta"><span><i class="fa fa-instagram"></i></span></a>
+            <a href="https://twitter.com/djkmeta"><span><i class="fa fa-twitter"></i></span></a>
+            <a href="https://soundcloud.com/djkmeta"><span><i class="fa fa-soundcloud"></i></span></a>
+          </div>
+        </div>
+      </div>
+
+      <div class="row media-elements">
+        <div class="col-sm-3 media-padding">
+          <div class="roster-img"><img src="./photos/haile-supreme.jpg" height="173.3" width="173.3" /></div>
+          <div>HAILE SUPREME</div>
+          <div>Recording Artist</div>
+          <div>
+            <a href="https://instagram.com/hailesupreme"><span><i class="fa fa-instagram"></i></span></a>
+            <a href="https://twitter.com/hailesupreme"><span><i class="fa fa-twitter"></i></span></a>
+            <a href="https://soundcloud.com/hailesupreme"><span><i class="fa fa-soundcloud"></i></span></a>
+          </div>
+        </div>
+        <div class="col-sm-3 media-padding">
+          <div class="roster-img"><img src="./photos/free-jacob-pic.jpg" height="173.3" width="173.3" /></div>
+          <div>FREE JACOB</div>
+          <div>Recording Artist</div>
+          <div>
+            <a href="https://instagram.com/free.jacob"><span><i class="fa fa-instagram"></i></span></a>
+            <a href="https://twitter.com/thefreejacob"><span><i class="fa fa-twitter"></i></span></a>
+            <a href="https://soundcloud.com/freejacob"><span><i class="fa fa-soundcloud"></i></span></a>
+          </div>
+        </div>
+        <div class="col-sm-3 media-padding">
+          <div class="roster-img"><img src="./photos/ibrahim-pic.jpg" height="173.3" width="173.3" /></div>
+          <div>IBRAHIM KEITA (of Les Autres)</div>
+          <div>Producer, Recording Artist</div>
+          <div>
+            <a href="https://www.instagram.com/jesuisibrahimkeita"><span><i class="fa fa-instagram"></i></span></a>
+            <a href="https://www.twitter.com/jesuisibrahimkeita"><span><i class="fa fa-twitter"></i></span></a>
+            <a href="https://soundcloud.com/jesuisibrahimkeita"><span><i class="fa fa-soundcloud"></i></span></a>
+          </div>
+        </div>
+        <div class="col-sm-3 media-padding">
+          <div class="roster-img"><img src="./photos/loh-pic.jpg" height="173.3" width="173.3"/></div>
+          <div>LOH</div>
+          <div>Recording Artist</div>
+          <div>
+            <a href="https://instagram.com/1wifloh"><span><i class="fa fa-instagram"></i></span></a>
+          </div>
+        </div>
+      </div>
+
+      <div class="row media-elements">
+        <div class="col-sm-3 media-padding">
+          <div class="roster-img"><img src="./photos/corbin-pic.jpg" height="173.3" width="150.5 " /></div>
+          <div>CORBIN BUTLER (of The Scofflaw)</div>
+          <div>Producer, Recording Artist</div>
+          <div>
+            <a href="https://instagram.com/CorbinHButler"><span><i class="fa fa-instagram"></i></span></a>
+            <a href="https://twitter.com/CorbinHButler"><span><i class="fa fa-twitter"></i></span></a>
+            <a href="https://soundcloud.com/corbin-butler"><span><i class="fa fa-soundcloud"></i></span></a>
+          </div>
+        </div>
+        <div class="col-sm-3 media-padding">
+          <div class="roster-img"><img src="./photos/jerms-pic.jpg" height="173.3" width="184"/></div>
+          <div>GOODDAY JERMS</div>
+          <div>Management</div>
+          <div>
+            <a href="https://instagram.com/GoodDayJerms"><span><i class="fa fa-instagram"></i></span></a>
+            <a href="https://twitter.com/GoodDayJerms"><span><i class="fa fa-twitter"></i></span></a>
+          </div>
+        </div>
+        <div class="col-sm-3 media-padding">
+          <div class="roster-img"><img src="./photos/solo-pic.jpg" height="173.3" width="166.6"/></div>
+          <div>SOLO</div>
+          <div>Management</div>
+          <div>
+            <a href="https://instagram.com/SoloThaBoss"><span><i class="fa fa-instagram"></i></span></a>
+            <a href="https://twitter.com/SoloThaBoss"><span><i class="fa fa-twitter"></i></span></a>
+          </div>
+        </div>
+
+      </div>
+
+
+      <img class="slizz-crew" src="/photos/slizzards-pic.jpg"/>
+
+      <footer> Webmaster: <a href="MAILTO:jon.rey22@gmail.com">Dr. Jrey</a> </footer>
+
+    </div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
