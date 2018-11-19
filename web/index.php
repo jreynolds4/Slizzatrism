1618bcfe3a5c196a3d574fb23ee8a19378fce3d3
jreynolds4 <jon.rey22@gmail.com>
2018-11-19 13:23:13 -0500

    Update index.php

--- web/index.php
+++ web/index.php
@@ -121,7 +121,7 @@
         <div class="col-sm-3"></div>
         <div class="col-sm-6">
           <p></p>
-          <img src="images/show-la-11-30.jpg" alt="LA Show">
+          <img src="images/show-la-11-30.jpg" alt="LA Show" width="100%" height="auto">
           <p></p>
           <p><button type="button" class="btn btn-success"><a href="https://www.eventbrite.com/e/ras-nebyu-live-on-sunset-blvd-tickets-52682487739">Click Here for Tickets</a></button></p>
         </div>
