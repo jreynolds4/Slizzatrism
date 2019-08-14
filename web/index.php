46ed5aa12ae6efc8497b008cc2ba0e4d7639604f
jreynolds4 <jon.rey22@gmail.com>
2019-08-14 08:37:52 -0400

    Added Days Later video

--- web/index.php
+++ web/index.php
@@ -133,6 +133,8 @@
       </div>
       
       <!-- END -->
+
+<div class="row centered"> <div class="col-sm-3"></div> <div class="col-sm-6"><hr></div> <div class="col-sm-3"></div> </div> <div class="row centered"> <div class="col-sm-3"></div> <div class="col-sm-6 lead">Ras Nebyu - Days Later (Official Music Video)</div> <div class="col-sm-3"></div> </div> <div class="row centered"> <div class="col-sm-3"></div> <div class="col-sm-6"> <p></p> <iframe width="100%" height="315" src="https://www.youtube.com/embed/BRReB5KStjo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> <p></p> </div> <div class="col-sm-3"></div> </div> <div class="row centered"> <div class="col-sm-3"></div> <div class="col-sm-6"><p></p></div> <div class="col-sm-3"></div> </div>
       
       <div class="row centered">
         <div class="col-sm-3"></div>
